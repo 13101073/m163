9242fa20c8a0171ac53921c1a81b0fba9c6990f3
molaga <13101073@users.noreply.github.com>
2017-03-30 23:53:48 +0800

    Update m163.js

--- chrome-plugin/js/m163.js
+++ chrome-plugin/js/m163.js
@@ -1,15 +1,14 @@
 (function(){
+
+	// m163 outchain player.
+	var ifr = '<iframe id="m163" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=465675149&auto=1&height=66"></iframe>';
+
+	var m163 = document.createElement('div');
+	m163.id = "m163";
+
+	// start building.
+	m163.innerHTML = ifr;		
+
+	document.body.appendChild(m163);
 	
-	//function(){
-		var ifr = '<iframe id="m163" frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=2&id=465675149&auto=1&height=66"></iframe>';
-		var m163 = document.createElement('div');
-		m163.id="m163";
-		m163.innerHTML = ifr;		
-		m163.style.position = "fixed";
-		m163.style.right = 0;
-		m163.style.bottom = 0;
-		m163.style.zIndex = 13163;
-		
-		document.body.appendChild(m163);		
-	// }
 })();
